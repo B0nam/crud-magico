42533b70a4ea8a73da653f7ab94b7bfa58dd4137
B0nam <daniiel.bonam@gmail.com>
2025-04-10 19:38:04 -0300

    chore: initial commit

---
+++ src/main/java/com/bonam/crud_magico/CrudMagicoApplication.java
@@ -0,0 +1,13 @@
+package com.bonam.crud_magico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMagicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMagicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonam/crud_magico/CrudMagicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonam.crud_magico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMagicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
